7070212480210b198475b1a931daa840aacd35bd
codethulu <brendanbell2001@hotmail.com>
2021-08-20 17:01:02 +0100

    added slash command functionality, and basis of some commands that have not been committed yet

--- bot.py
+++ bot.py
@@ -1,23 +1,54 @@
 # bot.py
 import discord
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
 import os
+from discord_slash.utils.manage_commands import create_option
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD_IDS= [687707946594992184]
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="!")
+slash = SlashCommand(bot, sync_commands=True)
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.content.startswith('ping'):
+        await message.reply('pong')
 
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
+@slash.slash(
+    name="test",
+    description="sends a message",
+    guild_ids=GUILD_IDS
+)
+async def _test(ctx: SlashContext):
+    await ctx.send("test complete!")
 
-client.run(TOKEN)
+# @slash.slash(
+#     name="generate_roles",
+#     description="sends a message",
+#     guild_ids=GUILD_IDS,
+#     options=[
+#         create_option(
+#             name="channel",
+#             description="Name of the channel message should be posted",
+#             required=True,
+#             option_type=7
+#         )
+#     ]
+# )
+# async def _generate_roles(ctx: SlashContext, channel:int):
+#     print(channel)
+#     ch=bot.get_channel(channel)
+#     await ch.ctx.send("role successfully generated!")
+
+
+bot.run(TOKEN)
